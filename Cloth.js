289480464f8b0418aef57c7fcd52cbd92ee255c6
john-mindrup <mindrup.john@gmail.com>
2022-11-29 11:50:43 -0600

    Cloth physics

--- Cloth.js
+++ Cloth.js
@@ -41,19 +41,40 @@ var vertexbuffer;
 
 // handle to the compiled shader program on the GPU
 var shader;
+//cloth physics properties
+var MASS = .1;
+var DRAG = .97;
+
+//cloth size properties
+var fabricLength = 600;
+var restDistance;
+var restDistanceB = 2;
+var restDistanceS = Math.sqrt(2);
+
+var initialBlanketPos = Blanket(500, 500);
+var blanket;
+
+function Blanket(width, height) {
+    return function (u, v) {
+        var x = u * width - width / 2;
+        var y = 125;
+        var z = v * height - height / 2;
+        return new THREE.Vector3(x, y, z);
+    }
+}
+
 
 
 function Particle(x, y, z, m)
 {
-  this.position = new THREE.Vector3(x, y, z);
-  this.previous = new THREE.Vector3(x, y, z);
-  this.original = new THREE.Vector3(x, y, z);
-
-  this.accel = new THREE.Vector3(0, 0, 0);
-  this.mass = m;
-  this.invMass = 1/this.mass;
-  this.tmp = new THREE.Vector3();
-  this.tmp2 = new THREE.Vector3();
+    this.position = initialBlanketPos(x, y);
+    this.previous = initialBlanketPos(x, y);
+    this.original = initialBlanketPos(x, y);
+    this.accel = new THREE.Vector3(0, 0, 0);
+    this.mass = m;
+    this.invMass = 1 / this.mass;
+    this.tmp = new THREE.Vector3();
+    this.tmp2 = new THREE.Vector3();
 
 }
 
@@ -64,43 +85,132 @@ Particle.prototype.addForce = function(force)
   );
 }
 
-Particle.prototype.integrate = function( timesq ) {
-
-  var newPos = this.tmp.subVectors(this.position, this.previous);
-  newPos.multiplyScalar(DRAG).add(this.position);
-  newPos.add( this.accel.multiplyScalar(timesq));
-
-  this.tmp = this.previous;
-  this.previous = this.position;
-  this.position = newPos;
-
-  this.accel.set(0, 0, 0);
+Particle.prototype.integrate = function (timesq) {
+    var newPos = this.tmp.subVectors(this.position, this.previous);
+    newPos.multiplyScalar(DRAG).add(this.position);
+    newPos.add(this.accel.multiplyScalar(timesq));
+    this.tmp = this.previous;
+    this.previous = this.position;
+    this.position = newPos;
+    this.accel.set(0, 0, 0);
+    
 
 };
 
-function satisifyConstrains( p1, p2, distance) {
-  var diff;
-  diff.subVectors( p2.position, p1.position );
-  var currentDist = diff.length();
-  if ( currentDist == 0 ) return; // prevents division by 0
-  var correction = diff.multiplyScalar( (currentDist - distance) / currentDist);
-  var correctionHalf = correction.multiplyScalar( 0.5 );
-  p1.position.add( correctionHalf );
-  p2.position.sub( correctionHalf );
+function satisifyConstrains(p1, p2, distance) {
+    var diff;
+    diff.subVectors(p2.position, p1.position);
+    var currentDist = diff.length();
+    if (currentDist == 0) return; // prevents division by 0
+    var correction = diff.multiplyScalar((currentDist - distance) / currentDist);
+    var correctionHalf = correction.multiplyScalar(0.5);
+    p1.position.add(correctionHalf);
+    p2.position.sub(correctionHalf);
 
 }
 
 function repelParticles( p1, p2, distance) {
-  var diff;
-  diff.subVectors( p2.position, p1.position );
-  var currentDist = diff.length();
-  if ( currentDist == 0 ) return; // prevents division by 0
-  if (currentDist < distance){
-    var correction = diff.multiplyScalar( (currentDist - distance) / currentDist);
-    var correctionHalf = correction.multiplyScalar( 0.5 );
-    p1.position.add( correctionHalf );
-    p2.position.sub( correctionHalf );
+    var diff;
+    diff.subVectors(p2.position, p1.position);
+    var currentDist = diff.length();
+    if (currentDist == 0) return; // prevents division by 0
+    if (currentDist < distance) {
+        var correction = diff.multiplyScalar((currentDist - distance) / currentDist);
+        var correctionHalf = correction.multiplyScalar(0.5);
+        p1.position.add(correctionHalf);
+        p2.position.sub(correctionHalf);
+
   }
+
+}
+
+function Cloth(width, height, length) {
+    this.width = width;
+    this.height = height;
+    restDistance = length / width;
+
+    var particles = [];
+    var constrains = [];
+
+    var u, v;
+
+    // Create particles
+    for (v = 0; v <= h; v++) {
+        for (u = 0; u <= w; u++) {
+            particles.push(
+                new Particle(u / w, v / h, 0, MASS)
+            );
+        }
+    }
+
+    for (v = 0; v <= h; v++) {
+        for (u = 0; u <= w; u++) {
+
+            if (v < h && (u == 0 || u == w)) {
+                constrains.push([
+                    particles[index(u, v)],
+                    particles[index(u, v + 1)],
+                    restDistance
+                ]);
+            }
+
+            if (u < w && (v == 0 || v == h)) {
+                constrains.push([
+                    particles[index(u, v)],
+                    particles[index(u + 1, v)],
+                    restDistance
+                ]);
+            }
+        }
+    }
+
+    //cross grain
+    for (v = 0; v < h; v++) {
+        for (u = 0; u < w; u++) {
+
+            if (u != 0) {
+                constrains.push([
+                    particles[index(u, v)],
+                    particles[index(u, v + 1)],
+                    restDistance
+                ]);
+            }
+
+            if (v != 0) {
+                constrains.push([
+                    particles[index(u, v)],
+                    particles[index(u + 1, v)],
+                    restDistance
+                ]);
+            }
+
+        }
+    }
+
+    //drape effect
+    for (v = 0; v < h; v++) {
+
+        for (u = 0; u < w; u++) {
+
+            if (v < h - 1) {
+                constrains.push([
+                    particles[index(u, v)],
+                    particles[index(u, v + 2)],
+                    restDistanceB * restDistance
+                ]);
+            }
+
+            if (u < w - 1) {
+                constrains.push([
+                    particles[index(u, v)],
+                    particles[index(u + 2, v)],
+                    restDistanceB * restDistance
+                ]);
+            }
+
+
+        }
+    }
 
 }
 
@@ -157,6 +267,7 @@ function main()
   // specify a fill color for clearing the framebuffer
   gl.clearColor(0.0, 0.8, 0.8, 1.0);
 
+    blanket = THREE.PlaneGeometry(500, 500, xSegs, ySegs);
   // define an animation loop
   var animate = function() {
 	draw();
